25415b53a6a8921c351ac51e6e8a287e632d8174
wwzlp-mac <610771202@qq.com>
2014-12-15 09:33:39 +0800

    12/13

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,15 +1,43 @@
 <link rel=" stylesheet" type="text/css" href="<?php echo base_url();?>assets/qianduan/components/signin.css">
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <!-- <div class="container">
-      <a class="navbar-brand" href="#">Question System</a>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Register</a></li>
-        <li><a href="#">Log in</a></li>
-      </ul>
-      
-        </div> -->
-  </div>
-    <div class="container" ng-controller="loginCtrl">
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">实验班出题系统</a>
+
+              <!-- <p class="navbar-text">功能-></p> -->
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <ul class="nav navbar-nav">
+            </ul>
+
+
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#" ><i class="glyphicon glyphicon-user"><span>请先登录</span></i></a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">设置 <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">注销</a></li>
+                  <li><a href="#">修改资料</a></li>
+                  <!-- <li><a href="#">Something else here</a></li> -->
+                  <li class="divider"></li>
+                  <li><a href="#">关于出题系统</a></li>
+                </ul>
+              </li>
+          </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+    </nav><!--top bar end-->
+    <div  ng-controller="loginCtrl">
       <form class="form-signin" role="form" ng-submit = "login()">
         <h2 class="form-signin-heading">Please sign in</h2>
         <input type="username" class="form-control" placeholder="Username" ng-model = "userName" required autofocus  >
