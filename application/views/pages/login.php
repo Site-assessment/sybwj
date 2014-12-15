c21d6cd26640aa8ca0008f487ba8a801c4f73350
wwzlp-mac <610771202@qq.com>
2014-12-15 19:28:00 +0800

    12/13

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,4 +1,12 @@
 <link rel=" stylesheet" type="text/css" href="<?php echo base_url();?>assets/qianduan/components/signin.css">
+<style type="text/css">
+  
+  body {
+  padding-top: 100px;
+    }
+
+</style>
+
         <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
             <div class="container-fluid">
               <!-- Brand and toggle get grouped for better mobile display -->
