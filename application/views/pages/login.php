eaa78cf5a2dc4cc983d9e1561df723bef013365e
wwzlp-mac <610771202@qq.com>
2014-12-15 09:50:07 +0800

    12/13

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -37,16 +37,16 @@
       </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
     </nav><!--top bar end-->
-    <div  ng-controller="loginCtrl" class="center-signin">
+    <div  ng-controller="loginCtrl">
       <form   class="form-signin" role="form" ng-submit = "login()">
-            <h2 class="form-signin-heading">请先登录</h2>
+            <h3 class="form-signin-heading">请先登录</h3>
             <input type="username" class="form-control" placeholder="Username" ng-model = "userName" required autofocus  >
             <input type="password" class="form-control" placeholder="Password"  ng-model = "password" required > 
             <!-- <a href="#">forget the password</a> -->
             <div class = "state1" ng-repeat="value in vm.values"><input type="radio" name="state" ng-model="vm.selection" ng-value="value"  ng-checked="vm.selection.state == value.state"/>{{value.statetext}}</div>
             </br>
-           <!-- <p ng-show = "loginError">用户名或密码错误</p>
-            <button class="btn btn-lg btn-primary btn-block" type="submit"  >Sign in</button> -->
+           <p ng-show = "loginError">用户名或密码错误</p>
+            <button class="btn btn-lg btn-primary btn-block" type="submit"  >Sign in</button>
       </form>
     </div> <!-- /container -->
   <script language="JavaScript">
